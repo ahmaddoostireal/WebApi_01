975c5e7fc546a7bf5570df6fa874c0870c8595dc
Ahmad Doosti <ahmaddoostireal@hotmail.com>
2019-05-13 19:15:25 +0430

    Add project files.

---
+++ WebApi_01/Models/ITodoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_01.Models
+{
+    public interface ITodoRepository
+    {
+        void Add(TodoItem item);
+        void Update(TodoItem item);
+
+        TodoItem Find(string Key);
+        TodoItem Remove(string Key);
+
+        IEnumerable<TodoItem> GetAll();
+
+    }
+}
---
+++ WebApi_01/Models/TodoRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Collections.Concurrent;
+
+namespace WebApi_01.Models
+{
+    public class TodoRepository : ITodoRepository
+    {
+
+        private static ConcurrentDictionary<string, TodoItem> _todos =
+            new ConcurrentDictionary<string, TodoItem>();
+
+
+        public TodoRepository()
+        {
+            Add(new Models.TodoItem { Name = "Item1" });
+        }
+
+        public void Add(TodoItem item)
+        {
+            item.Key = Guid.NewGuid().ToString();
+            _todos[item.Key] = item;
+        }
+
+        public void Update(TodoItem item)
+        {
+            _todos[item.Key] = item;
+        }
+
+        public TodoItem Find(string Key)
+        {
+            TodoItem item;
+            _todos.TryGetValue(Key, out item);
+            return item;
+        }
+        public TodoItem Remove(string Key)
+        {
+            TodoItem item;
+            _todos.TryRemove(Key, out item);
+            return item;
+        }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _todos.Values;
+        }
+  
+    }
+}
